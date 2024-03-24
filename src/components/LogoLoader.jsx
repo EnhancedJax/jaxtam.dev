2b7abc40940daacda22cc6994aabce4c30126fb6
Jax <enhancedjax@gmail.com>
2024-03-25 00:03:20 +0800

    Better loader

--- src/components/LogoLoader.jsx
+++ src/components/LogoLoader.jsx
@@ -6,15 +6,21 @@ const LogoLoader = () => {
       <style jsx>{`
         @keyframes coreanim {
           0% {
+            opacity: 0;
             transform: rotate(360deg);
           }
+          50% {
+            opacity: 1;
+            transform: rotate(180deg);
+          }
           100% {
+            opacity: 0;
             transform: rotate(0deg);
           }
         }
 
         img {
-          animation: coreanim 1s linear infinite;
+          animation: coreanim 1s cubic-bezier(0.445, 0.05, 0.55, 0.95) infinite;
         }
       `}</style>
       <img src="/images/iconlogo.png" className="w-16 h-16" />
