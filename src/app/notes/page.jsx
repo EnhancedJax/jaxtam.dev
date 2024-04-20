3119813b53121abd23313cb3119e97d862a6b638
Jax <enhancedjax@gmail.com>
2024-04-20 17:30:38 +0800

    Fixed dynamic content

--- src/app/notes/page.jsx
+++ src/app/notes/page.jsx
@@ -1,9 +1,9 @@
 import Content from "@comp/[notes]content";
 import { getNotes } from "@src/lib";
 
+export const dynamic = "force-dynamic";
+
 export default async function Notes() {
-    const notes = (await getNotes()) || [];
-    return (
-        <Content notes={notes}/>
-    )
+  const notes = (await getNotes()) || [];
+  return <Content notes={notes} />;
 }
