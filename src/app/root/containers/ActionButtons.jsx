78ba88201087e220b3303c2957f161aec55bf21d
Jax <enhancedjax@gmail.com>
2024-10-07 14:27:35 +0800

    Updated buttons to a tag

--- src/app/root/containers/ActionButtons.jsx
+++ src/app/root/containers/ActionButtons.jsx
@@ -19,33 +19,33 @@ const ActionButtons = () => {
         }
       }}
     >
-      <motion.div
+      <motion.a
         variants={slideLeft}
         initial="hidden"
         animate={slide ? "visible" : "hidden"}
         className="self-stretch h-[38px] px-11 py-2 cursor-pointer bg-pg rounded-lg justify-center items-center gap-2.5 flex border border-border "
         whileHover={{ scale: 1.05, translateY: -4 }}
         whileTap={{ scale: 0.95, translateY: -2 }}
-        onClick={() => window.open("mailto:jax.lytam@gmail.com")}
+        href="mailto:jax.lytam@gmail.com"
       >
-        <div className="text-base font-light text-bg ">Contact me</div>
+        <p className="text-base font-light text-bg ">Contact me</p>
         <Mail className="w-5 h-5 text-gray " />
-      </motion.div>
+      </motion.a>
       <div className="hidden font-light text-gray md:block">or</div>
-      <motion.div
+      <motion.a
         className="self-stretch h-[38px] px-11 py-2 cursor-pointer  bg-fg rounded-lg border  border-border justify-center items-center gap-2.5 hidden md:flex"
         variants={slideRight}
         initial="hidden"
         animate={slide ? "visible" : "hidden"}
         whileHover={{ scale: 1.05, translateY: -5 }}
         whileTap={{ scale: 0.95, translateY: -2 }}
-        onClick={() => window.open("/applications/Resume.pdf")}
+        href="/applications/Resume.pdf"
       >
-        <div className="text-base font-light text-pg " variants={slideUp}>
-          Snatch resume
-        </div>
+        <p className="text-base font-light text-pg " variants={slideUp}>
+          Get full resume
+        </p>
         <FileDown className="w-5 h-5 text-gray " />
-      </motion.div>
+      </motion.a>
     </motion.div>
   );
 };
