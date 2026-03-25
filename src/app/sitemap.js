9c091d8762e6d6e1a87506f2e5cda18927bcb8da
Jax Tam <80020581+EnhancedJax@users.noreply.github.com>
2026-03-25 19:57:50 +0800

    chore: remove thoughts

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,19 +1,10 @@
-import { getPosts } from "../lib";
-
 export default async function sitemap() {
   const baseUrl = "https://jaxtam.dev";
-
-  const posts = await getPosts();
-  const postUrls = posts.map((post) => ({
-    url: `${baseUrl}/thoughts/${post.node.slug}`,
-    lastModified: new Date(post.node.updatedAt),
-  }));
 
   return [
     {
       url: baseUrl,
       lastModified: new Date(),
     },
-    ...postUrls,
   ];
 }
--- src/app/thoughts/page.jsx
+++
@@ -1,7 +0,0 @@
-import { redirect } from "next/navigation";
-import { getFeaturedPostSlug } from "../../lib";
-
-export default async function ThoughtsIndex() {
-  const slug = await getFeaturedPostSlug();
-  redirect(`/thoughts/${slug}`);
-}
